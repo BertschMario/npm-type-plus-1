25b1f16e62276d17df99e80bec3913106aafcb91
Mario Bertsch <mario.bertsch@yoshie.io>
2022-04-23 14:21:37 +0200

    first functions

---
+++ playground.js
@@ -0,0 +1,10 @@
+﻿const boo = require('./dist')
+
+const testObj = {id:0,comment:'test'}
+const testArray = [{id:0,comment:'test1'},{id:1,comment:'test2'},{id:2,comment:'test3'}]
+
+console.log(boo.objectProperties(testObj))
+console.log(boo.arrayObjectValue(testArray, 'comment'))
+console.log(boo.arrayObjectValue(testArray, 'property'))
+console.log(boo.arrayGetFirst(testArray))
+console.log(boo.arrayGetLast(testArray))
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+export const objectProperties = (object: any) => {
+    return Object.keys(object);
+}
+export const arrayObjectValue = (array: any[], property:string) => {
+  if(array[0][property])
+    return array.map(x=>x[property]);
+  throwError(`property '${property}' dose not exist`, arrayObjectValue.name);
+  return [];
+}
+export const arrayGetFirst = (array: any[]) => {
+  return array.shift();
+}
+export const arrayGetLast = (array: any[]) => {
+  return array.pop();
+}
+
+function throwError(message:string, error?:any){
+  console.error(`[BetterObjectOperators${error ? '::'+error : ''}] ${message}`)
+}
